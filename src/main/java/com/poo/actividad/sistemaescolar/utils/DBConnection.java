80d43dbecc918680294bbf0235b3d31e413efa6e
Ander2211 <158358964+Ander2211@users.noreply.github.com>
2025-10-01 19:35:18 -0600

    Quitar contraseña

--- src/main/java/com/poo/actividad/sistemaescolar/utils/DBConnection.java
+++ src/main/java/com/poo/actividad/sistemaescolar/utils/DBConnection.java
@@ -7,10 +7,11 @@ import java.sql.DriverManager;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/sistema_gestion_academica"; // cambia si tu BD es distinta
     private static final String USER = "root"; // cambia si tu usuario es distinto
-    private static final String PASSWORD = "123"; // cambia si tienes password
+    private static final String PASSWORD = ""; // cambia si tienes password
 
     public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
+
